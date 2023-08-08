97870f2624b7fbe296f69b3cce4cbf23ef6b7ad6
Ni9er120404 <Niger120404@yandex.ru>
2023-08-08 23:00:30 +0300

    Added service for categories

---
+++ BlazorApplication/Server/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,44 @@
+﻿using BlazorApplication.Shared;
+
+namespace BlazorApplication.Server.Services.CategoryService
+{
+	public class CategoryService : ICategoryService
+	{
+		public List<Category> Categories { get; set; } = new()
+		{
+			new Category()
+				{
+					Id=1,
+					Name = "Fantasy",
+					Url="fantasy",
+					Icon="fantasy"
+				},
+				new Category()
+				{
+					Id=2,
+					Name = "Science Fiction",
+					Url="science-fiction",
+					Icon="science-fiction"
+				},
+				new Category()
+				{
+					Id=3,
+					Name = "Horror",
+					Url="horror",
+					Icon="horror"
+				}
+		};
+
+		public async Task<List<Category>> GetAllCategories()
+		{
+			return Categories;
+		}
+
+		public async Task<Category> GetCategoryByUrl(string categoryUrl)
+		{
+			Category? category = Categories.FirstOrDefault(category => category.Url.ToLower() == categoryUrl.ToLower());
+
+			return category;
+		}
+	}
+}
---
+++ BlazorApplication/Server/Services/CategoryService/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using BlazorApplication.Shared;
+
+namespace BlazorApplication.Server.Services.CategoryService
+{
+	public interface ICategoryService
+	{
+		public Task<List<Category>> GetAllCategories();
+
+		public Task<Category> GetCategoryByUrl(string categoryUrl);
+	}
+}
