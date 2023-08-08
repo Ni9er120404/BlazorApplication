8bbb7bf75dd6ad721e8a52280b2c4720946826a6
Ni9er120404 <Niger120404@yandex.ru>
2023-08-08 17:05:07 +0300

    Added a service for working with products

---
+++ BlazorApplication/Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using BlazorApplication.Shared;
+
+namespace BlazorApplication.Client.Services.ProductService
+{
+	public interface IProductService
+	{
+		public List<Product> Products { get; set; }
+
+		public void LoadProducts();
+	}
+}
