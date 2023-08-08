016145a9491e1ac4e01fdddf7e805e32a7f4e599
Ni9er120404 <Niger120404@yandex.ru>
2023-08-08 23:25:54 +0300

    Updated old and added new packages

--- BlazorApplication/Server/Program.cs
+++ BlazorApplication/Server/Program.cs
@@ -3,42 +3,42 @@ using BlazorApplication.Server.Services.ProductService;
 
 namespace BlazorApplication.Server
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-            _ = builder.Services.AddControllersWithViews();
-            _ = builder.Services.AddRazorPages();
+			_ = builder.Services.AddControllersWithViews();
+			_ = builder.Services.AddRazorPages();
 
-            _ = builder.Services.AddScoped<IProductService, ProductService>();
-            _ = builder.Services.AddScoped<ICategoryService, CategoryService>();
+			_ = builder.Services.AddScoped<IProductService, ProductService>();
+			_ = builder.Services.AddScoped<ICategoryService, CategoryService>();
 
-            using WebApplication app = builder.Build();
+			using WebApplication app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseWebAssemblyDebugging();
-            }
-            else
-            {
-                _ = app.UseExceptionHandler("/Error");
-                _ = app.UseHsts();
-            }
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseWebAssemblyDebugging();
+			}
+			else
+			{
+				_ = app.UseExceptionHandler("/Error");
+				_ = app.UseHsts();
+			}
 
-            _ = app.UseHttpsRedirection();
+			_ = app.UseHttpsRedirection();
 
-            _ = app.UseBlazorFrameworkFiles();
-            _ = app.UseStaticFiles();
+			_ = app.UseBlazorFrameworkFiles();
+			_ = app.UseStaticFiles();
 
-            _ = app.UseRouting();
+			_ = app.UseRouting();
 
-            _ = app.MapRazorPages();
-            _ = app.MapControllers();
-            _ = app.MapFallbackToFile("index.html");
+			_ = app.MapRazorPages();
+			_ = app.MapControllers();
+			_ = app.MapFallbackToFile("index.html");
 
-            app.Run();
-        }
-    }
+			app.Run();
+		}
+	}
 }
