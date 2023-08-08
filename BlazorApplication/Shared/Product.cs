5c3e6f78f7beed8fec9352bf3ac712228fae9c43
Ni9er120404 <Niger120404@yandex.ru>
2023-08-08 15:50:15 +0300

    A product model has been created

---
+++ BlazorApplication/Shared/Product.cs
@@ -0,0 +1,29 @@
+﻿namespace BlazorApplication.Shared
+{
+	public class Product
+	{
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public string Image { get; set; }
+
+		public decimal Price { get; set; }
+
+		public decimal OriginalPrice { get; set; }
+
+		public bool IsPublic { get; set; }
+
+		public bool IsDeleted { get; set; }
+
+		public int CategoryId { get; set; }
+
+		public Category Category { get; set; }
+
+		public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+		public DateTime UpdatedDate { get; set; } = DateTime.Now;
+	}
+}
