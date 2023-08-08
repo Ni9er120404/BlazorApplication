7e266fcd8a42912b4f420e6e99ab92071ab2501e
Ni9er120404 <Niger120404@yandex.ru>
2023-08-08 17:11:01 +0300

    Added support for services

--- BlazorApplication/Client/Program.cs
+++ BlazorApplication/Client/Program.cs
@@ -1,4 +1,5 @@
-using BlazorApplication.Client;
+using BlazorApplication.Client.Services.CategoryService;
+using BlazorApplication.Client.Services.ProductService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,11 +9,14 @@ namespace BlazorApplication.Client
 	{
 		public static async Task Main(string[] args)
 		{
-			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 			builder.RootComponents.Add<App>("#app");
 			builder.RootComponents.Add<HeadOutlet>("head::after");
 
-			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			_ = builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			_ = builder.Services.AddScoped<IProductService, ProductService>();
+			_ = builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 			await builder.Build().RunAsync();
 		}
