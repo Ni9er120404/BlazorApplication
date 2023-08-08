a53e7665a0077738f71c38585294061cf49708af
Ni9er120404 <Niger120404@yandex.ru>
2023-08-08 23:01:02 +0300

    Added service for products

---
+++ BlazorApplication/Server/Services/ProductService/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using BlazorApplication.Shared;
+
+namespace BlazorApplication.Server.Services.ProductService
+{
+    public interface IProductService
+    {
+        public Task<List<Product>> GetAllProducts();
+
+        public Task<List<Product>> GetProductsByCategory(string categoryUrl);
+
+        public Task<Product> GetProductById(int id);
+    }
+}
