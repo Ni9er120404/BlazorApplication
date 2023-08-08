f2b1ba420aa9d6a020930c769ca7ef70323e50f9
Ni9er120404 <Niger120404@yandex.ru>
2023-08-08 17:05:23 +0300

    Added a service for working with categories

---
+++ BlazorApplication/Client/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,37 @@
+﻿using BlazorApplication.Shared;
+
+namespace BlazorApplication.Client.Services.CategoryService
+{
+	public class CategoryService : ICategoryService
+	{
+		public List<Category> Categories { get; set; } = new();
+
+		public void LoadCategories()
+		{
+			Categories = new List<Category>()
+			{
+				new Category()
+				{
+					Id=1,
+					Name = "Fantasy",
+					Url="fantasy",
+					Icon="fantasy"
+				},
+				new Category()
+				{
+					Id=2,
+					Name = "Science Fiction",
+					Url="science-fiction",
+					Icon="science-fiction"
+				},
+				new Category()
+				{
+					Id=3,
+					Name = "Horror",
+					Url="horror",
+					Icon="horror"
+				}
+			};
+		}
+	}
+}
---
+++ BlazorApplication/Client/Services/CategoryService/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using BlazorApplication.Shared;
+
+namespace BlazorApplication.Client.Services.CategoryService
+{
+	public interface ICategoryService
+	{
+		public List<Category> Categories { get; set; }
+
+		public void LoadCategories();
+	}
+}
