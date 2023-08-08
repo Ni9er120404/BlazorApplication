5e5d90511f9160570af8334076c650387989a4a1
Ni9er120404 <Niger120404@yandex.ru>
2023-08-08 23:04:07 +0300

    Added a controller for categories

---
+++ BlazorApplication/Server/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using BlazorApplication.Server.Services.CategoryService;
+using BlazorApplication.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApplication.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Category>>> GetCategories()
+        {
+            var categories = await _categoryService.GetAllCategories();
+
+            return Ok(categories);
+        }
+    }
+}
